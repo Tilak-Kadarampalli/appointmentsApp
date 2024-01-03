d29e6b0d0573ebdc684a0d5937b11ca5c62967e2
Tilak Kadarampalli <123760271+Tilak-Kadarampalli@users.noreply.github.com>
2024-01-03 17:05:25 +0530

    Updated appItem index.js

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,17 @@
 // Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetials} = props
+  const {id, title, date, isStarred} = appointmentDetials
+
+  return (
+    <div>
+      <p>{title}</p>
+      <p>{date}</p>
+      <p>{isStarred}</p>
+    </div>
+  )
+}
+
+export default AppointmentItem
